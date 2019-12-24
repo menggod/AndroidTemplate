b6dd38cb97e3b85b8b3aa4600b0ea9fc73fa2e4f
menggod <menggod@126.com>
2019-12-24 11:17:21 +0800

    init

---
+++ app/src/main/java/com/example/template/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.template;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * @author yiche
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
